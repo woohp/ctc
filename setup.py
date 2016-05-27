676d2e2f893dae394b4613e28c02dbb0f5dcde45
Hui Peng Hu <woohp135@gmail.com>
2016-05-27 10:14:44 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ctc',
+    version='0.1',
+    description='Connectionist Temporal Classification Loss',
+    setup_requires=["cffi>=1.0.0"],
+    cffi_modules=["ctc_build.py:ffi"],
+    install_requires=["cffi>=1.0.0"],
+)
