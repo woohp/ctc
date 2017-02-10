700abf0b60e8149bbca6bbc4ae1407203ef185d3
Hui Peng Hu <woohp135@gmail.com>
2017-02-10 15:13:00 -0800

    remove unnecessary __restrict__ in header

--- src/ctc.h
+++ src/ctc.h
@@ -2,46 +2,46 @@ extern "C"
 {
 
 void ctc(
-    const float* __restrict__ const y,
-    const unsigned* __restrict__ const l,
+    const float* const y,
+    const unsigned* const l,
     const unsigned batches,
     const unsigned timesteps,
     const unsigned alphabet_size,
     const unsigned labels_length,
-    float* __restrict__ const losses,
-    float* __restrict__ const grad);
+    float* const losses,
+    float* const grad);
 
 void ctc_loss_only(
-    const float* __restrict__ const y,
-    const unsigned* __restrict__ const labels,
+    const float* const y,
+    const unsigned* const labels,
     const unsigned batches,
     const unsigned timesteps,
     const unsigned alphabet_size,
     const unsigned labels_length,
-    float* __restrict__ const losses);
+    float* const losses);
 
 unsigned decode(
-    const float* __restrict__ const y,
+    const float* const y,
     const unsigned timesteps,
     const unsigned alphabet_size,
-    unsigned* __restrict__ const out);
+    unsigned* const out);
 
 void equals(
-    const float* __restrict__ const y_pred,
-    const unsigned* __restrict__ const labels,
+    const float* const y_pred,
+    const unsigned* const labels,
     const unsigned batches,
     const unsigned timesteps,
     const unsigned alphabet_size,
     const unsigned labels_length,
-    unsigned char* __restrict__ const out);
+    unsigned char* const out);
 
 void character_error_rate(
-    const float* __restrict__ const y_pred,
-    const unsigned* __restrict__ const labels,
+    const float* const y_pred,
+    const unsigned* const labels,
     const unsigned batches,
     const unsigned timesteps,
     const unsigned alphabet_size,
     const unsigned labels_length,
-    float* __restrict__ const out);
+    float* const out);
 
 }
